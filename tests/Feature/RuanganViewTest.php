2bee54ac433cefc6754b210d479426954435ea7c
RafaMufid <akurafamufid@gmail.com>
2026-04-26 17:02:08 +0700

    fix: change test cases using mock data

--- tests/Feature/RuanganViewTest.php
+++ tests/Feature/RuanganViewTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Support\ViewErrorBag;
+use Illuminate\Support\Facades\Http;
 
 class RuanganViewTest extends TestCase
 {
@@ -14,9 +15,23 @@ class RuanganViewTest extends TestCase
         view()->share('errors', new ViewErrorBag);
     }
 
+    private function getBaseUrl()
+    {
+        return config('api.base_url');
+    }
+
     public function test_halaman_index_ruangan_berhasil_dimuat()
     {
-        $response = $this->withSession(['user' => ['role' => 'admin']])
+        $baseUrl = $this->getBaseUrl();
+
+        Http::fake([
+            $baseUrl . '/api/rooms' => Http::response([
+                ['id' => 1, 'name' => 'Ruang Teori 1', 'floor' => '1', 'capacity' => 30]
+            ], 200),
+            $baseUrl . '/api/bookings' => Http::response([], 200),
+        ]);
+
+        $response = $this->withSession(['api_token' => 'mock-token'])
                          ->get('/ruangan');
         
         $response->assertStatus(200);
@@ -25,7 +40,14 @@ class RuanganViewTest extends TestCase
 
     public function test_terdapat_input_pencarian_untuk_javascript()
     {
-        $response = $this->withSession(['user' => ['role' => 'admin']])
+        $baseUrl = $this->getBaseUrl();
+
+        Http::fake([
+            $baseUrl . '/api/rooms' => Http::response([], 200),
+            $baseUrl . '/api/bookings' => Http::response([], 200),
+        ]);
+
+        $response = $this->withSession(['api_token' => 'mock-token'])
                          ->get('/ruangan');
         
         $response->assertSee('id="roomSearch"', false);
@@ -33,7 +55,14 @@ class RuanganViewTest extends TestCase
 
     public function test_terdapat_dropdown_filter_status_ruangan()
     {
-        $response = $this->withSession(['user' => ['role' => 'admin']])
+        $baseUrl = $this->getBaseUrl();
+
+        Http::fake([
+            $baseUrl . '/api/rooms' => Http::response([], 200),
+            $baseUrl . '/api/bookings' => Http::response([], 200),
+        ]);
+
+        $response = $this->withSession(['api_token' => 'mock-token'])
                          ->get('/ruangan');
         
         $response->assertSee('id="roomStatusFilter"', false);
@@ -41,9 +70,42 @@ class RuanganViewTest extends TestCase
 
     public function test_tampilan_saat_data_ruangan_kosong_menampilkan_pesan_yang_sesuai()
     {
-        $response = $this->withSession(['user' => ['role' => 'admin']])
+        $baseUrl = $this->getBaseUrl();
+
+        // return array kosong
+        Http::fake([
+            $baseUrl . '/api/rooms' => Http::response([], 200),
+            $baseUrl . '/api/bookings' => Http::response([], 200),
+        ]);
+
+        $response = $this->withSession(['api_token' => 'mock-token'])
                          ->get('/ruangan');
 
         $response->assertSee('Belum ada ruangan tersedia');
+    }
+
+    public function test_tampilan_saat_data_ruangan_terisi_menampilkan_nama_ruangan()
+    {
+        $baseUrl = $this->getBaseUrl();
+
+        Http::fake([
+            $baseUrl . '/api/rooms' => Http::response([
+                [
+                    'id' => 1, 
+                    'name' => 'Laboratorium Komputer', 
+                    'floor' => '2', 
+                    'capacity' => 25, 
+                    'facilities' => 'AC, LAN'
+                ]
+            ], 200),
+            $baseUrl . '/api/bookings' => Http::response([], 200),
+        ]);
+
+        $response = $this->withSession(['api_token' => 'mock-token'])
+                         ->get('/ruangan');
+
+        $response->assertStatus(200);
+        $response->assertSee('Laboratorium Komputer');
+        $response->assertDontSee('Belum ada ruangan tersedia');
     }
 }
